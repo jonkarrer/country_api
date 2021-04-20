6a091489a7492c288b57881bb57d5e4bfeb91aa7
jonkarrer <karrerjon@gmail.com>
2021-04-19 20:40:00 -0500

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 
-export let base = "/country_api/";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
